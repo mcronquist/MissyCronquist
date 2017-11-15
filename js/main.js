fc39624b7744f4e2cafcb0ca1bd7fac2302ceb41
Melissa Cronquist <melissacronquist@Melissas-MacBook-Pro.local>
2017-11-14 19:12:36 -0800

    updates

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var app = angular.module('tutorialWebApp', [
   app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       // Home
-      .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
+      .when("/", {templateUrl: "partials/index.html", controller: "PageCtrl"})
       // Pages
       .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
       .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
